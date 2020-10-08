44c42719f74b949b3220bed002b7e183de6f17a7
Christopher <christopher.warren@evanscycles.com>
2020-10-08 09:57:10 +0100

    Modified HelloWorld App

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hellow world, this is my Java project");
+		System.out.println("I'm adding code to my project");
 	}
 
 }
